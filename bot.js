969ccaf5b452b97a00ed29ac779e2d87a4518897
peter <hyght2type@gmail.com>
2018-12-04 10:57:30 -0800

    new bot

---
+++ bot.js
@@ -0,0 +1,28 @@
+var twit = require('twit');
+
+var config = require('./config');
+
+var T = new twit(config);
+
+setInterval(tweetIt, 1000*60*60);
+
+tweetIt();
+
+function tweetIt() {
+
+    var r = Math.floor(Math.random()*100);
+
+    var tweet = {
+        status: 'here is a random number ' + r + ' #hyght2type'
+    }
+
+    
+
+        T.post('statuses/update', tweet, function(err, data, response) { 
+            if (err) {
+            console.log("Something went wrong")
+            } else {
+                console.log("It worked!")
+            }
+          })
+        }
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    consumer_key:         'ZyMFsSU5jYERBxMYDxyDGkK1a'
+  , consumer_secret:      'KkRgq3lAyX1bYVSxk52r8zyCpBlF5avXYXLsC2dGu4w7LYR7jd'
+  , access_token:         '19323617-ck7XZ929yLQVY0PHWtqBNuYw2mG0tlncxjVwSNFIN'
+  , access_token_secret:  'BrZWcpmIH8A1mUsBlJ7kXrlwkOyFyed63D7ftkFCyFIUR'
+  
+}
